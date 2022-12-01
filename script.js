e080f3abe0e0eb1e69d09dd94da4644d8bc9c4ef
mend1s <andremendesskt99@gmail.com>
2022-11-30 22:43:22 -0300

    refatoração function async

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
-var consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/')
-    .then(resposta => resposta.json())
-    .then(r => {
-        if(r.erro){
-            throw Error('Esse cep não existe!')
-        } else {
-            console.log(r)
-        }
-    })
-    .catch(erro => console.log(erro))
-    .finally(mensagem => console.log('Processamento concluído!'));
+async function BuscaCEP(){
+    var consultaCEP = await fetch('https://viacep.com.br/ws/01001000/json/')
+    var consultaCEPConvertida = await consultaCEP.json();
 
-console.log(consultaCEP);
+    console.log(consultaCEPConvertida)
+}
+
+BuscaCEP();
